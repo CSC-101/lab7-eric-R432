fb219974ea6f06fd2305c7e2c8020e7db43bc37f
eric-R432 <156847504+eric-R432@users.noreply.github.com>
2024-11-11 19:27:15 -0800

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,23 @@
+from typing import Optional
 
+def str_to_float_two(a_str: str) -> Optional[float]:
+    try:
+        return str_to_float_two(a_str)
+    except ValueError:
+        return None
+def gather_numbers():
+    numbers = []
+
+    while True:
+        user_input = input("Enter a number, type done to end")
+        if user_input == 'done':
+            break
+
+        number = str_to_float_two(user_input)
+        if number is not None:
+            numbers.append(number)
+    return numbers
+
+if __name__ == '__main__':
+    numbers = gather_numbers()
+    print("collected numbers", numbers)
