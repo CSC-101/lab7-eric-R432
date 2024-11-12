ffb3cf79516cfe217478b8cedf84dfbacb39dd35
eric-R432 <156847504+eric-R432@users.noreply.github.com>
2024-11-11 19:26:58 -0800

    Update convert_tests.py

--- convert_tests.py
+++ convert_tests.py
@@ -1,1 +1,17 @@
+import unittest
 
+from convert import str_to_float
+
+
+class test_str_to_float(unittest.TestCase):
+        def test_one(self):
+            input = '123.2'
+            expected = 123.2
+            result = str_to_float(input)
+            self.assertEqual(expected,result)
+
+        def test_one_2(self):
+            input = 'xyz'
+            expected = None
+            result = str_to_float(input)
+            self.assertEqual(expected, result)
